3570c8628f91b17782055c5adcce7296e25a9ebf
bangyuli <by.brad.li@gmail.com>
2024-11-27 12:25:22 +0800

    Fixed implementation for tutorial 06

--- CSC8503CoreClasses/OrientationConstraint.cpp
+++ CSC8503CoreClasses/OrientationConstraint.cpp
@@ -22,13 +22,14 @@ void OrientationConstraint::UpdateConstraint(float dt) {
 	Quaternion qB = objectB->GetTransform().GetOrientation();
 	PhysicsObject* physA = objectA->GetPhysicsObject();
 	PhysicsObject* physB = objectB->GetPhysicsObject();
-
+	float invMassA = physA->GetInverseMass();
+	float invMassB = physB->GetInverseMass();
+	float constraintMass = invMassA + invMassB;
 	//计算相对旋转
 	Quaternion relativeRotation = qB.Conjugate() * qA;
 	relativeRotation.Normalise();
 	//计算旋转角度
 	float angle = 2.0f * acos(std::clamp(relativeRotation.w, -1.0f, 1.0f));
-
 	if (angle > maxRotationRadians) {
 		float correctionFactor = maxRotationRadians / angle;
 		//修正旋转到最大允许角度
@@ -39,11 +40,7 @@ void OrientationConstraint::UpdateConstraint(float dt) {
 		if (Vector::Length(correctionAxis) > 0.0f) {
 			correctionAxis = Vector::Normalise(correctionAxis);
 			float correctionAngle = angle - maxRotationRadians;
-
 			// 根据惯性计算角动量修正
-			float invMassA = physA->GetInverseMass();
-			float invMassB = physB->GetInverseMass();
-			float constraintMass = invMassA + invMassB;
 
 			if (constraintMass > 0.0f) {
 				Vector3 angularImpulse = correctionAxis * (correctionAngle / constraintMass);
